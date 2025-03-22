14a623ea4bcb40f35eff09516878f44420ff4699
Juhan Tamm <juhan.tamm@nortal.com>
2025-03-22 14:19:17 +0200

    Initial setup commit

---
+++ src/main/java/com/lhv/sanctionedpeopledetector/SanctionedPeopleDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.lhv.sanctionedpeopledetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanctionedPeopleDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanctionedPeopleDetectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lhv/sanctionedpeopledetector/SanctionedPeopleDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhv.sanctionedpeopledetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanctionedPeopleDetectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
